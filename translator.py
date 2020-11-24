6ea4760b1f5919b2fb5ea7337f242f7ce9a8cb63
bharanivijay69 <65061664+bharanivijay69@users.noreply.github.com>
2020-11-24 23:44:22 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,10 +1,9 @@
 from translate import Translator
 import pickle
 to_lang = 'fr'
-secret = '5deadcc9f92542aba66edd0cbfa0fbc3'
+secret = '5deadcc9f92542aba66edd0cbfa0fbc3'     #security key from microsof azure cloud for using translation service
 translator = Translator(provider='microsoft', to_lang=to_lang, secret_access_key=secret)
-word_file_name=input("ENTER FILE NAME:")
-word_file=open(word_file_name)
+word_file=open("find_words.txt")                #text file which contains the english words that are needed to be translated to french
 word_list=[]
 for words in word_file:
     temp=words.rstrip('\n')
@@ -13,14 +12,7 @@ frenchdict=dict()
 for word in word_list:
     result=translator.translate(word)
     frenchdict[word]=result
-print(frenchdict)
-file=open('frenchdictionary','wb')
-pickle.dump(frenchdict,file)
+print(frenchdict)                               #translated english words are stored in dictionary form
+file=open('frenchdictionary','wb')              
+pickle.dump(frenchdict,file)                    #dictionary is dumped to a file called finaldictionary
 file.close()
-file_to_read = open("frenchdictionary", "rb")
-final_dictionary = pickle.load(file_to_read)
-print(final_dictionary)
-count=0
-for words in final_dictionary:
-    count=count+1
-print("\n The number of words translated is ",count)
